b22df911632373495bf48314d7ff755ffcc6331e
ACER <ACER@DESKTOP-O7A416V>
2022-10-21 23:45:04 +0300

    Добавьте файлы проекта.

---
+++ Program (1).cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace ConsoleApp52
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите x: ");
+            double x = double.Parse(Console.ReadLine());
+            double y = x > 6.7 ? 4 - Math.Exp(4 * x) : Math.Log(3.5 + x);
+            Console.WriteLine($"Результат = {y}");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Program (3).cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace aip;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Введите Х=");
+        double x = double.Parse(Console.ReadLine());
+
+        Console.WriteLine("Введите Y=");
+
+        double y = double.Parse(Console.ReadLine());
+
+        /*    if (x >= -3 && x <= 3 && y <= 3 && y >= 0)
+                Console.WriteLine("Точка попадает в заштрихованную область");
+            else
+                Console.WriteLine("Точка не попадает в заштрихованную область");*/
+
+
+        double r = Math.Pow(Math.Pow(3, 2) + Math.Pow(3, 2), 0.5);
+
+        double testR = Math.Pow(Math.Pow(x, 2) + Math.Pow(y, 2), 0.5);
+
+        if (testR < r && x >= -3 && x <= 3 && y <= 3 && y >= 0)
+            Console.WriteLine("Точка попадает в заштрихованную область");
+        else
+            Console.WriteLine("Точка не попадает в заштрихованную область");
+    }
+}
